782c2af40684d9d9db948c9eddbb8bb2e8d9c00d
zxqcel <113905655+zxqcel@users.noreply.github.com>
2024-08-05 18:12:58 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -30,7 +30,7 @@ set -e
 
 export GITHUB_SOURCE="v1.1.0"
 export SCRIPT_RELEASE="v1.1.0"
-export GITHUB_BASE_URL="https://raw.githubusercontent.com/pterodactyl-installer/pterodactyl-installer"
+export GITHUB_BASE_URL="https://raw.githubusercontent.com/zxqcel/i"
 
 LOG_PATH="/var/log/pterodactyl-installer.log"
 
